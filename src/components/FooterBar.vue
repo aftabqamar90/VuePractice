b9afdfe6597f482a7183ff3102ab7d36d4315e3d
Qamar Aftab <qamar.aftab@indistech.com>
2022-07-11 14:09:04 +0500

    custom components
    Add custom components

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        My Footer
+    </div>
+</template>
+<script>
+
+export default{
+    name :'FooterBar'
+}
+</script>
---
+++ src/components/PostItem.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="card">
+            <div class="card-content">
+                <h4 class="title is-6">
+                {{post.name}}
+                </h4>
+            </div>
+        </div>
+</template>
+<script>
+export default {
+    name: 'PostItem',
+    props:["post"]
+};
+</script>
