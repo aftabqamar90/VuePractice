5902d63febe8c81f1110a40bcb98020ab06a84aa
Qamar Aftab <qamar.aftab@indistech.com>
2022-07-12 10:59:13 +0500

    Comuniation
    Communication between 2 components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
  <div id="app">
   Hello world
+  <SayHello @onSayHello="HelloMethod"></SayHello>
   </div>
 </template>
 
 <script>
 import   "bulma/css/bulma.css"
+import  SayHello from "./components/SayHello"
 export default {
   name: 'App',
   components: {
+    SayHello
   },
+  methods: {
+    HelloMethod(){
+      alert('Say hello');
+    },
+  },    
 };
 </script>
---
+++ src/components/SayHello.vue
@@ -0,0 +1,8 @@
+<template>
+     <button @click="$emit('onSayHello')">Say Hello</button>
+</template>
+<script>
+export default {
+    name:'SayHellow'
+}
+</script>
