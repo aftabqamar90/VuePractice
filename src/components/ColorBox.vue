e630fd01d7fadd18567e38e0878f61f01b4617ee
Qamar Aftab <qamar.aftab@indistech.com>
2022-07-12 12:30:54 +0500

    Change  Color with Custom Event

---
+++ src/components/ColorBox.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="box box-color" @click="$emit('ChangeColor',color)" :style="{'background-color':color}"></div>
+</template>
+<script>
+
+export default {
+    name:'ColorBox',
+    props:['color']
+}
+</script>
